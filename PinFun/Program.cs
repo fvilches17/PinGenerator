67778bc3f23c9b2e1a415a2b6af21d319c2cc803
Francisco Vilches <f_vilches17@outlook.com>
2018-10-31 05:28:14 +1300

    Add project files.

---
+++ PinFun/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PinFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
